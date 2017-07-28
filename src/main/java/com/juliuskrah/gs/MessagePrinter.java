e729a4fadb4f0a07840488962b96f90906539f96
Julius Krah <juliuskrah@gmail.com>
2017-07-28 23:14:38 +0000

    Updated code to use 'Spring 4.3' updated features Automaic constructor injection. If you have just one constructor in a component it's parameters are automatically injected. No need to add '@Inject'

--- src/main/java/com/juliuskrah/gs/MessagePrinter.java
+++ src/main/java/com/juliuskrah/gs/MessagePrinter.java
@@ -1,14 +1,11 @@
 package com.juliuskrah.gs;
 
-import javax.inject.Inject;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessagePrinter {
 	private MessageService messageService;
 
-	@Inject
 	public MessagePrinter(MessageService messageService) {
 		this.messageService = messageService;
 	}
